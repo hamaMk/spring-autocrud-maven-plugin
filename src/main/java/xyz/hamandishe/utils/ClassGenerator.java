f76d33ff71bca56454f017854abb0efec86a0b90
hmkandabvute <hmkandabvute@posb.co.zw>
2023-10-02 10:33:16 +0200

    feat: Add default method for saving generated source file

--- src/main/java/xyz/hamandishe/utils/ClassGenerator.java
+++ src/main/java/xyz/hamandishe/utils/ClassGenerator.java
@@ -1,8 +1,20 @@
 package xyz.hamandishe.utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public interface ClassGenerator {
     FileUtils fileUtils = new FileUtils();
+
     void generate(String className);
 
-    void save();
+    default void save(Path filepath, List<String> classContent) {
+        try {
+            Files.write(filepath, classContent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
